495e7bead32433b562225a3a2c5cebe57349d0f3
mildc0ffee <uncxochtil@gmail.com>
2019-06-09 22:51:51 +0900

    RENAME package name

--- src/ConsoleInput.jl
+++ src/ConsoleInput.jl
@@ -1,4 +1,4 @@
-module Input
+module ConsoleInput
 
 export readInt
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,36 @@
-using Test, Input
+using Test, ConsoleInput
 
 origin_stdin = stdin
 test_in = open("./test.txt", "r")
 redirect_stdin(test_in)
 
 @testset "DlmType" begin
-    @test isa(' ', Input.DlmType)
-    @test isa("abc", Input.DlmType)
-    @test isa(r"^\s*(?:#|$)", Input.DlmType)
+    @test isa(' ', ConsoleInput.DlmType)
+    @test isa("abc", ConsoleInput.DlmType)
+    @test isa(r"^\s*(?:#|$)", ConsoleInput.DlmType)
 
     function dlm()::String
             "false"
     end
-    @test isa(dlm(), Input.DlmType)
+    @test isa(dlm(), ConsoleInput.DlmType)
 end
 
 @testset "readInt" begin
-    @test Input.readInt(test_in) == 1
-    @test Input.readInt(test_in) == [1, 2, 3, 4, 5]
-    @test Input.readInt(test_in, ",") == [6, 7, 8, 9, 10]
+    @test ConsoleInput.readInt(test_in) == 1
+    @test ConsoleInput.readInt(test_in) == [1, 2, 3, 4, 5]
+    @test ConsoleInput.readInt(test_in, ",") == [6, 7, 8, 9, 10]
 end
 
 @testset "readString" begin
-    @test Input.readString(test_in) == "Lorem"
-    @test Input.readString(test_in) == ["Lorem", "ipsum", "es", "simplemente"]
-    @test Input.readString(test_in, ";") == ["consectetur", "adipiscing", "elit"]
+    @test ConsoleInput.readString(test_in) == "Lorem"
+    @test ConsoleInput.readString(test_in) == ["Lorem", "ipsum", "es", "simplemente"]
+    @test ConsoleInput.readString(test_in, ";") == ["consectetur", "adipiscing", "elit"]
 end
 
 @testset "readGeneral" begin
-    @test Input.readGeneral(Float64, test_in) == 0.0012
-    @test Input.readGeneral(Complex{Float64}, test_in) == 0.32 + 4.5im
-    @test Input.readGeneral(Complex{Int}, test_in) == [1+5im, 10-4im, -9+8im]
+    @test ConsoleInput.readGeneral(Float64, test_in) == 0.0012
+    @test ConsoleInput.readGeneral(Complex{Float64}, test_in) == 0.32 + 4.5im
+    @test ConsoleInput.readGeneral(Complex{Int}, test_in) == [1+5im, 10-4im, -9+8im]
 end
 close(test_in)
 redirect_stdin(origin_stdin)
